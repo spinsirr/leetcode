f0636edbe216be3bb19be793438ec803adf505d9
spenc <spencerzhyp@gmail.com>
2023-08-13 14:36:37 +0800

    finished Leetcode 20

---
+++ src/stack/ValidParentheses20.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @FileName: ValidParentheses20
+ * @Description: TODO
+ * @Author: Spencer ZHAO
+ * @Date: 8/13/2023
+ */
+public class ValidParentheses20 {
+    
+    public boolean isValid(String s) {
+        Stack<Character> chars = new Stack<>();
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        for (char e : s.toCharArray()) {
+            if (e == '(') {
+                chars.push(')');
+            } else if (e == '[') {
+                chars.push(']');
+            } else if (e == '{') {
+                chars.push('}');
+            } else if (chars.isEmpty() || chars.peek() != e) {
+                return false;
+            } else {
+                chars.pop();
+            }
+        }
+        return chars.isEmpty();
+    }
+}
