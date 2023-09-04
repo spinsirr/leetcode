220dd972f4d312ff3713de66233a1a53a09d5484
spenc <spencerzhyp@gmail.com>
2023-09-05 00:38:13 +0800

    Binary Tree Traverse

--- src/binarytree/CountCompleteTreeNodes222.java
+++ src/binarytree/CountCompleteTreeNodes222.java
@@ -12,6 +12,21 @@ public class CountCompleteTreeNodes222 {
         if (node == null) {
             return 0;
         }
+        TreeNode left = node.left;
+        TreeNode right = node.right;
+        int leftDepth = 0;
+        int rightDepth = 0;
+        while (left != null) {
+            left = left.left;
+            leftDepth++;
+        }
+        while (right != null) {
+            right = right.right;
+            rightDepth++;
+        }
+        if (leftDepth == rightDepth) {
+            return (2 << leftDepth) - 1;
+        }
         int leftNum = getNum(node.left);
         int rightNum = getNum(node.right);
         int res = leftNum + rightNum + 1;
